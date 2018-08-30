4bb7203fac5528044788bdae813a3fd6539f9853
Tracy Williams <selpilot@gmail.com>
2018-08-29 19:03:10 -0700

    updated README and completed mailgun integration

--- src/routes/orderRoutes.js
+++ src/routes/orderRoutes.js
@@ -2,6 +2,7 @@
 // routes for ordering what's in the cart
 //
 import stripe from 'stripe';
+import Mailgun from 'mailgun-js';
 
 import _data from '../lib/data';
 import config from '../config';
@@ -27,6 +28,11 @@ handlers._order = {};
 // Required data: a cart file and a valid token
 // Optional data: none
 handlers._order.post = (data, cb) => {
+  // validate the card token the client should have received from stripe
+  const cardToken = typeof data.payload.cardToken === 'string' && data.payload.cardToken.length > 0
+    ? data.payload.cardToken : false;
+  if (!cardToken) return cb(400, { Error: 'Invalid Strip charge card token' });
+
   // get the token from the header
   const token = typeof data.headers.token === 'string' ? data.headers.token : false;
   // read the token data
@@ -47,17 +53,35 @@ handlers._order.post = (data, cb) => {
       myStripe.charges.create({
         amount: cartTotal,
         currency: 'usd',
-        source: config.TEST_STRIPE_TOKEN, // obtained with Stripe.js
+        source: cardToken, // obtained with Stripe.js
         description: `Pizza charge for ${tokenData.email}`,
       }, (err, charge) => {
         if (err) return cb(500, { Error: `Stripe API error:\n${err}` }); 
         const { status, amount } = charge;
-        return cb(200, { status, amount });
+
+        // charge succeeded, send customer email receipt
+        const mailgun = new Mailgun({ apiKey: config.MAILGUN_PRIVATE_API_KEY, domain: config.MAILGUN_DOMAIN });
+
+        const cartMessageString = cartData.reduce((str, item) => `${str}<tr><td>${item.name}</td><td>${item.size}</td><td>${item.qty}</td><td>${item.price}</td><td>${item.extPrice}</td></tr>`, '');
+
+        const message = {
+          from: 'selpilot@gmail.com',
+          to: tokenData.email, 
+          subject: 'Receipt for your recent Pizza order',
+          html: `<p>Thank you for your recent order!  Here is your receipt:</p><table><tr><th>Item</th><th>Size</th></th><th>Qty</th><th>Price</th><th>Extended Price</th></tr>${cartMessageString}<tr><td colspan=4>Total:</td><td>${cartTotal / 100}</td></tr>`,
+        };
+        mailgun.messages().send(message, (mgerr, body) => {
+          if (mgerr) return cb(200, { Error: `Charge successful but receipt email failed: ${mgerr}` });
+          // Else respond with 200 and charge status info
+          return cb(200, { chargeStatus: status, chargeAmount: amount / 100, emailStatus: body.message });
+        });
+        return undefined;
       });
       return undefined;
     });
     return undefined;
   });
+  return undefined;
 };
 
 export default handlers;
